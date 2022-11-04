5f604ae7a137360d50f7e04568d5457eef60c35f
Mehmet Sait Taşdemir <108357127+MhmdSait@users.noreply.github.com>
2022-11-04 15:14:25 +0300

    Update MethodPage.java

--- HepsiburadaFinalCase3/src/main/java/com/mycompany/pages/MethodPage.java
+++ HepsiburadaFinalCase3/src/main/java/com/mycompany/pages/MethodPage.java
@@ -16,36 +16,7 @@ public class MethodPage extends PageBase {
 		
 	}
 	
-	public MethodPage loginAs() throws InterruptedException{
-		
-		
-		js.highlight(userName, driver); js.drawBorder(userName, driver);
-		userName.sendKeys("testuser1"); Thread.sleep(2000);
-		js.highlight(passWord, driver); js.drawBorder(passWord, driver); Thread.sleep(2000); 
-		passWord.sendKeys("SeleniumTestAccount1"); 		
-		js.highlight(rememberMe, driver); js.drawBorder(rememberMe, driver);
-		rememberMe.click(); Thread.sleep(2000);
-		js.highlight(loginButton, driver); js.drawBorder(loginButton, driver);
-		loginButton.click(); Thread.sleep(2000);		
-		
-		return new MethodPage(driver);
-		
-	}
-					
-		
-	public MethodPage loginYou() throws InterruptedException{
-		
-		userName.sendKeys("testuser1"); Thread.sleep(2000);
-		passWord.sendKeys("SeleniumTestAccount1"); Thread.sleep(2000);
-		rememberMe.click(); Thread.sleep(2000);
-		loginButton.click(); Thread.sleep(2000);
-		
-		
-		return new MethodPage(driver);
-		
-	}
 	
-
 	public MethodPage hepsiburada01() throws InterruptedException{
 		
 		
@@ -54,17 +25,14 @@ public class MethodPage extends PageBase {
 		giris1.click();
 		js.drawBorder(giris2, driver);
 		giris2.click(); Thread.sleep(1000);
-		
 		faceGiris.click(); Thread.sleep(1000);
-		
-		faceMail.sendKeys("mail_giriniz");
-		
-		faceSifre.sendKeys("şifre-giriniz");
+		faceMail.sendKeys("mailgiriniz");
+		faceSifre.sendKeys("şifre");
 		js.drawBorder(faceSifre, driver);
 		faceSifre.sendKeys(Keys.ENTER); Thread.sleep(1000);
 		
-		//Assert.assertTrue(logoElementi.isDisplayed());
 		Assert.assertTrue(logoElementi.isDisplayed());
+		Assert.assertTrue(üyeAdi.isDisplayed()); js.drawBorder(üyeAdi, driver);
 		
 		urunArama.sendKeys("çorap");
 		urunArama.sendKeys(Keys.ENTER); Thread.sleep(1000);
@@ -73,7 +41,6 @@ public class MethodPage extends PageBase {
 		//js.clickElementByJS(corap, driver); 
 		System.out.println("sayfa başligi: " + js.getTitleByJS(driver)); 
 		corap.click(); Thread.sleep(1000);
-		
 		
 		String parentWindow= driver.getWindowHandle();
 		
@@ -90,7 +57,6 @@ public class MethodPage extends PageBase {
 		ikinciUrun.click(); js.drawBorder(ikinciUrun, driver); Thread.sleep(1000);
 		
 		js.scrollIntoView(sayfaBasi, driver); js.drawBorder(sayfaBasi, driver);Thread.sleep(1000);
-		
 		js.refreshBrowserByJS(driver); Thread.sleep(2000);
 		
 		sepetim.click(); Thread.sleep(1000);
@@ -98,24 +64,51 @@ public class MethodPage extends PageBase {
 		Assert.assertTrue(urun1.isDisplayed()); js.drawBorder(urun1, driver);
 		Assert.assertTrue(urun2.isDisplayed()); js.drawBorder(urun2, driver);
 		
-		
-		
-		
 		//Assertions.isTrueArgument(getTitle(), true);
-		
 				
 		return new MethodPage(driver);
 		
 	}
 					
-					
+		
+	
+	public MethodPage hepsiburada02() throws InterruptedException{
+		
+		js.highlight(cerezler, driver); js.drawBorder(cerezler, driver);
+		cerezler.click(); Thread.sleep(1000);
+		urunArama.sendKeys("yün çorap");
+		urunArama.sendKeys(Keys.ENTER); Thread.sleep(1000);
+		
+		js.scrollIntoView(yunCorap, driver); js.drawBorder(yunCorap, driver);
+		System.out.println("sayfa başligi: " + js.getTitleByJS(driver));
+		yunCorap.click(); Thread.sleep(1000);
+		
+		String parentWindow= driver.getWindowHandle();
+		
+		Set<String> allWindows= driver.getWindowHandles();
+		
+			for(String curWindow: allWindows) {
+				driver.switchTo().window(curWindow);
+			}
+		sepetEkle.click(); js.drawBorder(sepetEkle, driver); Thread.sleep(1000);
+		js.scrollIntoView(sepetEkle, driver); Thread.sleep(1000);	
+			
+		ikinciUrun.click(); js.drawBorder(ikinciUrun, driver); Thread.sleep(1000);
+		
+		js.scrollIntoView(sayfaBasi, driver); js.drawBorder(sayfaBasi, driver);Thread.sleep(1000);
+		js.refreshBrowserByJS(driver); Thread.sleep(2000);
+		
+		sepetim.click(); Thread.sleep(1000);
+		
+		Assert.assertTrue(satici1.isDisplayed()); js.drawBorder(satici1, driver);
+		Assert.assertTrue(satici2.isDisplayed()); js.drawBorder(satici2, driver);
+		
+		return new MethodPage(driver);
+		
+	}
+	
 	
  	}
 	
 
 	
-	
-	
-	
-	
-
