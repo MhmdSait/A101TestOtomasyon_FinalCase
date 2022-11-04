c58655d45dd6eedd6b65c2f768be8baab9b5a3cf
Mehmet Sait Taşdemir <108357127+MhmdSait@users.noreply.github.com>
2022-11-04 15:08:34 +0300

    Test02 eklendi

--- HepsiburadaFinalCase3/src/test/java/com/mycompany/tests/CallTests.java
+++ HepsiburadaFinalCase3/src/test/java/com/mycompany/tests/CallTests.java
@@ -1,6 +1,5 @@
 package com.mycompany.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -9,27 +8,8 @@ import org.testng.annotations.Test;
 public class CallTests extends TestBase{
 	
 
-	 @Test //(priority=1)
-	  public void loginFunction() throws InterruptedException{ 
-		    extentTest = extent.startTest("Login validation"); //test result screenshot almasi icin
-			homePage.clickMyAccount().loginAs()			
-			;			
-			
-	  }
-
-		
-	 @Test //(priority=1)
-	  public void loginFunction1() throws InterruptedException{ 
-		    extentTest = extent.startTest("Login validation"); //test result screenshot almasi icin
-			homePage.clickMyAccount().loginAs()
-			;				
-			
-			
-	  }
-	 
-
-
-	 @Test //(priority=1)
+	
+	 @Test (priority=1)
 	  public void hepsiburadaTest01() throws InterruptedException{ 
 		    extentTest = extent.startTest("Login validation"); //test result screenshot almasi icin
 			homePage.clickMyAccount()
@@ -40,7 +20,17 @@ public class CallTests extends TestBase{
 
 		
 		
-	 
+
+	 @Test (priority=2)
+	  public void hepsiburadaTest02() throws InterruptedException{ 
+		    extentTest = extent.startTest("Login validation"); //test result screenshot almasi icin
+			homePage.clickMyAccount()
+			.hepsiburada02()
+			;			
+			
+	  }
+
+		
 	 
 	 
   }
