66714ee4b06e35c68d3789f7cf79b30a7ca58ee3
Mehmet Sait Taşdemir <108357127+MhmdSait@users.noreply.github.com>
2022-11-06 23:20:01 +0300

    Update CallTests.java

--- HepsiburadaFinalCase3/src/test/java/com/mycompany/tests/CallTests.java
+++ HepsiburadaFinalCase3/src/test/java/com/mycompany/tests/CallTests.java
@@ -1,107 +1,135 @@
-package com.mycompany.pages;
+package com.mycompany.tests;
 
-import java.util.Set;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
 
-import org.openqa.selenium.Keys;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import com.mycompany.pages.HomePage;
+import com.mycompany.util.Browser;
+import com.mycompany.util.JSEHelper;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class TestBase {
+
+	protected WebDriver driver;
+	Logger logger = Logger.getLogger(TestBase.class);
+
+	protected static Properties testConfig;
+	public ExtentReports extent;
+	public ExtentTest extentTest;
+	protected HomePage homePage;
+	JSEHelper js = new JSEHelper();
+
+	@BeforeSuite
+	public void beforeSuit() throws FileNotFoundException, IOException {
+		testConfig = new Properties();
+		testConfig.load(new FileInputStream("TestConfig.properties"));
+	}
+
+	@BeforeMethod
+	public void beforeMethod() throws MalformedURLException {
+		driver = Browser.createDriver(testConfig.getProperty("browser"));
+		driver.get(testConfig.getProperty("baseUrl"));
+		homePage = new HomePage(driver);
+		// loginPage.loginHome(testConfig.getProperty("username"),
+		// testConfig.getProperty("password"));
+		System.out.println(homePage.getTitle());
+		logger.info("Hepsiburada sitesine gidildi.");
+
+	}
+
+	@BeforeTest
+	public void setExtent() {
+		logger.info("Test baslatildi.");
+		extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/ExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Lenovo");
+		extent.addSystemInfo("Team", "QA Team");
+		extent.addSystemInfo("Environment", "QA");
+
+	}
+
+	@AfterTest
+	public void endReport() {
+		extent.flush();
+		extent.close();
+		logger.info("Test tamamlandi.");
+	}
+
+	public static String getScreenshotForFail(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		// after execution, you could see a folder "FailedTestsScreenshots"
+		// under src folder
+		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
+				+ ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+	}
+
+	public static String getScreenshotForSuccess(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		// after execution, you could see a folder "FailedTestsScreenshots"
+		// under src folder
+		String destination = System.getProperty("user.dir") + "/SuccessTestsScreenshots/" + screenshotName + dateName
+				+ ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+	}
+
+	/*
+	 * public void login() { loginPage.loginHome(testConfig.getProperty("username"),
+	 * testConfig.getProperty("password")); }
+	 */
 
 
-public class MethodPage extends PageBase {
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException, InterruptedException {
 
-	
-	public MethodPage(WebDriver driver) 
-	{
-		super(driver);
+		if (result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
+			extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in
+																							// extent report
+
+			String screenshotPath = getScreenshotForFail(driver, result.getName());
+			extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath)); // to add screenshot in extent
+																							// report
+			// extentTest.log(LogStatus.FAIL, extentTest.addScreencast(screenshotPath));
+			// //to add screencast/video in extent report
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			extentTest.log(LogStatus.SKIP, "Test Case SKIPPED IS " + result.getName());
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+			extentTest.log(LogStatus.PASS, "Test Case PASSED IS " + result.getName());
+
+			String screenshotPath = getScreenshotForSuccess(driver, result.getName()); // 1
+			extentTest.log(LogStatus.PASS, extentTest.addScreenCapture(screenshotPath)); // to add screenshot in extent
+																							// report //2
+		}
+
+		extent.endTest(extentTest); // ending test and ends the current test and prepare to create html report
+		Browser.quitDriver(driver);
 		
 	}
-	
-	
-	public MethodPage hepsiburada01() throws InterruptedException{
-		
-		
-		js.highlight(cerezler, driver); js.drawBorder(cerezler, driver);
-		cerezler.click(); Thread.sleep(1000);
-		giris1.click();
-		js.drawBorder(giris2, driver);
-		giris2.click(); Thread.sleep(1000);
-		faceGiris.click(); Thread.sleep(1000);
-		faceMail.sendKeys("m.said04@hotmail.com");
-		faceSifre.sendKeys("Said201162-");
-		js.drawBorder(faceSifre, driver);
-		faceSifre.sendKeys(Keys.ENTER); Thread.sleep(1000);
-		
-		Assert.assertTrue(logoElementi.isDisplayed());
-		Assert.assertTrue(üyeAdi.isDisplayed()); js.drawBorder(üyeAdi, driver);
-		
-		urunArama.sendKeys("çorap");
-		urunArama.sendKeys(Keys.ENTER); Thread.sleep(1000);
-		
-		js.scrollIntoView(corap, driver); js.drawBorder(corap, driver); 
-		System.out.println("sayfa başligi: " + js.getTitleByJS(driver)); 
-		corap.click(); Thread.sleep(1000);
-		
-		String parentWindow= driver.getWindowHandle();
-		
-		Set<String> allWindows= driver.getWindowHandles();
-		
-			for(String curWindow: allWindows) {
-				driver.switchTo().window(curWindow);
-			}
-			
-		sepetEkle.click(); js.drawBorder(sepetEkle, driver); Thread.sleep(1000);
-		js.scrollIntoView(sepetEkle, driver); Thread.sleep(1000);
-		
-		ikinciUrun.click(); js.drawBorder(ikinciUrun, driver); Thread.sleep(1000);
-		
-		js.scrollIntoView(sayfaBasi, driver); js.drawBorder(sayfaBasi, driver); Thread.sleep(1000);
-		js.refreshBrowserByJS(driver); Thread.sleep(1000);
-		
-		sepetim.click(); Thread.sleep(1000);
-		
-		Assert.assertTrue(urun1.isDisplayed()); js.drawBorder(urun1, driver);
-		Assert.assertTrue(urun2.isDisplayed()); js.drawBorder(urun2, driver);
-		
-				
-		return new MethodPage(driver);
-		
-	}
-					
-		
-	
-	public MethodPage hepsiburada02() throws InterruptedException{
-		
-		js.highlight(cerezler, driver); js.drawBorder(cerezler, driver);
-		cerezler.click(); Thread.sleep(1000);
-		urunArama.sendKeys("yün çorap");
-		urunArama.sendKeys(Keys.ENTER); Thread.sleep(1000);
-		
-		js.scrollIntoView(yunCorap, driver); js.drawBorder(yunCorap, driver);
-		yunCorap.click(); Thread.sleep(1000);
-		
-		String parentWindow= driver.getWindowHandle();
-		
-		Set<String> allWindows= driver.getWindowHandles();
-		
-			for(String curWindow: allWindows) {
-				driver.switchTo().window(curWindow);
-			}
-		sepetEkle.click(); js.drawBorder(sepetEkle, driver); Thread.sleep(1000);
-		js.scrollIntoView(sepetEkle, driver); Thread.sleep(2000);
-			
-		ikinciUrun.click(); js.drawBorder(ikinciUrun, driver); Thread.sleep(1000);
-		
-		js.scrollIntoView(sayfaBasi, driver); js.drawBorder(sayfaBasi, driver); Thread.sleep(1000);
-		js.refreshBrowserByJS(driver); Thread.sleep(2000);
-		
-		sepetim.click(); Thread.sleep(1000);
-		
-		Assert.assertTrue(satici1.isDisplayed()); js.drawBorder(satici1, driver);
-		Assert.assertTrue(satici2.isDisplayed()); js.drawBorder(satici2, driver);
-		
-		return new MethodPage(driver);
-		
-	}
-	
-	
- }
+}
